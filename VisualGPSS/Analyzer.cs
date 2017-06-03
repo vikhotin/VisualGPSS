2955c2cb899a2e12bc233079f44e4502ba38d60e
vikhotin <vikhotin@yandex.ru>
2017-06-03 08:12:23 +0300

    ANOVA form and class added

---
+++ VisualGPSS/Analyzer.cs
@@ -0,0 +1,18 @@
+﻿using MathWorks.MATLAB.NET.Utility;
+using MathWorks.MATLAB.NET.Arrays;
+using MatlabMancovan;
+
+namespace VisualGPSS
+{
+    static class Analyzer
+    {
+        public static void Anova(double[,] groups, double[,] y)
+        {
+            MWNumericArray Groups = new MWNumericArray(groups);
+            MWNumericArray Y = new MWNumericArray(y);
+
+            Mancovan mancova = new Mancovan();
+            MWArray res = mancova.mancovan(Groups, Y);
+        }
+    }
+}
---
+++ VisualGPSS/AnovaForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+
+namespace VisualGPSS
+{
+    public partial class AnovaForm : Form
+    {
+        public AnovaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog(this);
+            edtFilename.Text = openFileDialog.FileName;
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            double[,] groups;
+            double[,] Y;
+            try
+            {
+                using (StreamReader reader = new StreamReader(edtFilename.Text))
+                {
+                    string s = reader.ReadLine();
+                    int[] ints = s.Split(' ').Select(t => Convert.ToInt32(t)).ToArray();
+
+                    int dataLength = ints[0];
+                    int IVNumber = ints[1];
+                    int DVNumber = ints[2];
+
+                    groups = new double[dataLength, IVNumber];
+                    Y = new double[dataLength, DVNumber];
+
+                    for (int i = 0; i < dataLength; i++)
+                    {
+                        s = reader.ReadLine();
+                        double[] nums = s.Split(' ')
+                                         .Select(t => double.Parse(t, CultureInfo.InvariantCulture))
+                                         .ToArray();
+                        for (int j = 0; j < IVNumber; j++)
+                        {
+                            groups[i, j] = nums[j];
+                        }
+                        for (int j = 0; j < DVNumber; j++)
+                        {
+                            Y[i, j] = nums[IVNumber + j];
+                        }
+                    }
+                }
+
+                Analyzer.Anova(groups, Y);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
